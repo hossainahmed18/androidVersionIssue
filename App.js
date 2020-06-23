30ea49c9c7cee61592dc959aae454ca785950a78
junayed <junayed@intellier.com>
2020-06-23 09:12:53 +0600

    new project ready for solving android versioning issue

---
+++ App.js
@@ -0,0 +1,40 @@
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text
+} from 'react-native';
+
+
+
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+componentDidMount() {
+}
+
+  render(){
+    return (
+      <ScrollView>
+         <Text>New Project Ready</Text>
+       </ScrollView>
+    
+    );
+  }
+  
+}
+const styles = StyleSheet.create({
+  
+
+});
+
+
